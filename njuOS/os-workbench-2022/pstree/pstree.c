9a025074b1ada58c96aac75a1aa7a002909fbd00
tracer-nju <tracer@nju.edu.cn>
2022-07-05 22:29:36 +0800

    Linux jasonj2021-virtual-machine 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:29:36 up  3:23,  5 users,  load average: 0.23, 0.10, 0.03

--- njuOS/os-workbench-2022/pstree/pstree.c
+++ njuOS/os-workbench-2022/pstree/pstree.c
@@ -3,50 +3,73 @@
 #include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-
-int is_digits_composed(char * s , int len);
+int is_digits_composed(char *s, int len);
 int is_digit(char c);
 
 /**
- * @brief 
+ * @brief
  * -p, --show-pids: 打印每个进程的进程号。
  * -n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。
  * -V --version: 打印版本信息。
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   char p = 0; // 打印每个进程的进程号
   char n = 0; // 按照pid的大小输出
   char v = 0; // 打印版本信息
   // ===================================命令行参数处理==========================================
-  for (int i = 0; i < argc; i++) {
+  for (int i = 0; i < argc; i++)
+  {
     assert(argv[i]);
-    if(strcmp(argv[i] , "-V") == 0 || strcmp(argv[i] , "--version") == 0){
+    if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0)
+    {
       v = 1;
     }
-    if(strcmp(argv[i] , "-n") == 0 || strcmp(argv[i] , "--numeric-sort") == 0){
-      n= 1;
+    if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0)
+    {
+      n = 1;
     }
-    if(strcmp(argv[i] , "-p") == 0 || strcmp(argv[i] , "--show-pids") == 0){
+    if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0)
+    {
       p = 1;
     }
     printf("argv[%d] = %s\n", i, argv[i]);
   }
-  printf("p = %d , n = %d , v = %d\n" , p , n , v );
+  printf("p = %d , n = %d , v = %d\n", p, n, v);
   assert(!argv[argc]);
   // ==================================读取/proc=====================================================
-  DIR * proc = opendir("/proc");
+  DIR *proc = opendir("/proc");
   struct dirent *dr = readdir(proc);
-  
-  while(dr != NULL){
+
+  while (dr != NULL)
+  {
     // printf("%s\n" , dr->d_name);
-    if(is_digits_composed(dr->d_name , 256)){
-      printf("process name = %d\n" , atoi(dr->d_name));
+    if (is_digits_composed(dr->d_name, 256))
+    {
+      FILE *fp = fopen(strcat("/proc/" , dr->d_name ), 'r');
+      pid_t pid;
+      char comm[256];
+      char state;
+      pid_t ppid;
+      memset(comm,0,256);
+      if(fp){
+        fscanf(fp , "%d %s %c %d" , &pid , comm , state, &ppid)
+      }else{
+        close(fp);
+        perror("File reading error \n");
+        exit(1);
+      }
+      close(fp);
+      printf("process name = %d\n", atoi(dr->d_name));
+      printf("pid = %d , process name = %s , state = %c , ppid = %d" , pid , comm , state , ppid);
     }
     dr = readdir(proc);
   }
@@ -55,21 +78,29 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int is_digits_composed(char * s , int len){
-  int i ;
-  for(i = 0 ; i < len ; i++){
-    if(!is_digit(s[i])){
-      if(s[i] == '\0'){
+int is_digits_composed(char *s, int len)
+{
+  int i;
+  for (i = 0; i < len; i++)
+  {
+    if (!is_digit(s[i]))
+    {
+      if (s[i] == '\0')
+      {
         return 1;
-      }else{
+      }
+      else
+      {
         return 0;
       }
     }
   }
   return 1;
 }
-int is_digit(char c){
-  if (c <= '9' && c >= '0'){
+int is_digit(char c)
+{
+  if (c <= '9' && c >= '0')
+  {
     return 1;
   }
   return 0;
