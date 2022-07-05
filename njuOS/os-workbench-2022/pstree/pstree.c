ce0d598f36fff57eed8d136a99d3966a062c4427
tracer-nju <tracer@nju.edu.cn>
2022-07-05 20:58:48 +0800

    Linux jasonj2021-virtual-machine 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:58:48 up  1:52,  2 users,  load average: 0.06, 0.05, 0.00

--- njuOS/os-workbench-2022/pstree/pstree.c
+++ njuOS/os-workbench-2022/pstree/pstree.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+/**
+ * @brief 
+ * -p, --show-pids: 打印每个进程的进程号。
+ * -n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。
+ * -V --version: 打印版本信息。
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 
 int main(int argc, char *argv[]) {
+  unsigned char p = 0; // 打印每个进程的进程号
+  unsigned char n = 0; // 按照pid的大小输出
+  unsigned char v = 0; // 打印版本信息
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
+    if(strcmp(argv[i] , "-V") == 0 || strcmp(argv[i] , "--version") == 0){
+      v = 1;
+    }
+    if(strcmp(argv[i] , "-n") == 0 || strcmp(argv[i] , "--numeric-sort") == 0){
+      n= 1;
+    }
+    if(strcmp(argv[i] , "-V") == 0 || strcmp(argv[i] , "--version") == 0){
+      p = 1;
+    }
+
+
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  printf("p = %c , n = %c , v = %c" , p , n , v );
   assert(!argv[argc]);
   return 0;
 }
