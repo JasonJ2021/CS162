665416ce9d34782f7ea6736d9377a8918839cab8
tracer-nju <tracer@nju.edu.cn>
2022-07-09 23:24:49 +0800

    Linux jasonj2021-virtual-machine 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:24:49 up  3:08,  4 users,  load average: 0.06, 0.06, 0.00

--- njuOS/os-workbench-2022/pstree/pstree.c
+++ njuOS/os-workbench-2022/pstree/pstree.c
@@ -11,9 +11,8 @@ int is_digits_composed(char *s, int len);
 int is_digit(char c);
 void truncate_right_bracket(char *s);
 
-
 // 变量
-static int p = 0; // 打印每个进程的进程号 
+static int p = 0; // 打印每个进程的进程号
 static int n = 0; // 按照pid的大小输出
 static int v = 0; // 打印版本信息
 static struct _proc *list = NULL;
@@ -21,12 +20,12 @@ static struct _proc *list = NULL;
 typedef struct _proc
 {
   // 进程的结构定义
-  pid_t pid;    //进程的Pid
-  pid_t ppid;   //父进程的pid
-  char comm[64]; // 进程的名字
-  struct _proc * parent; // 父进程
-  struct _proc * next;   // 用一个链表表示所有进程
-  struct _child * children; // 用一个链表表示进程的所有子进程
+  pid_t pid;               //进程的Pid
+  pid_t ppid;              //父进程的pid
+  char comm[64];           // 进程的名字
+  struct _proc *parent;    // 父进程
+  struct _proc *next;      // 用一个链表表示所有进程
+  struct _child *children; // 用一个链表表示进程的所有子进程
 } PROC;
 
 typedef struct _child
@@ -35,7 +34,6 @@ typedef struct _child
   struct _proc *child;
   struct _child *next;
 } CHILD;
-
 
 /**
  * @brief
@@ -51,8 +49,8 @@ typedef struct _child
 int main(int argc, char *argv[])
 {
   char temp[261];
-  memset(temp , 0 , 261);
-  strcpy(temp , "/proc/");
+  memset(temp, 0, 261);
+  strcpy(temp, "/proc/");
 
   // ===================================命令行参数处理==========================================
   for (int i = 0; i < argc; i++)
@@ -77,49 +75,51 @@ int main(int argc, char *argv[])
   // ==================================读取/proc=====================================================
   DIR *proc = opendir("/proc");
   struct dirent *dr = readdir(proc);
-  
-  
+
   // 遍历一个文件夹
   while (dr != NULL)
   {
-    memset(temp , 0 , 261);
-    strcpy(temp , "/proc/");
-    printf("%s\n" ,strcat(temp , dr->d_name ) );
+    memset(temp, 0, 261);
+    strcpy(temp, "/proc/");
+    printf("%s\n", strcat(temp, dr->d_name));
     if (is_digits_composed(dr->d_name, 256))
     {
-      strcat(temp , "/stat");
+      strcat(temp, "/stat");
       FILE *fp = fopen(temp, "r");
       pid_t pid;
       char comm[66];
       char state;
       pid_t ppid;
-      memset(comm,0,66);
+      memset(comm, 0, 66);
       // 获取进程号和父进程号
-      if(fp){
+      if (fp)
+      {
         char buffer[1000];
-        memset(buffer , 0 , 1000);
-        fgets(buffer ,1000 , fp);
-        char *token = strtok(buffer , " ");
+        memset(buffer, 0, 1000);
+        fgets(buffer, 1000, fp);
+        char *token = strtok(buffer, " ");
         pid = atoi(token);
-        token = strtok(NULL , " ");
+        token = strtok(NULL, " ");
         // 获取进程名称
-        strcpy(comm,token);
-        token = strtok(NULL , " ");
-        token = strtok(NULL , " ");
+        strcpy(comm, token);
+        token = strtok(NULL, " ");
+        token = strtok(NULL, " ");
         // 获取父进程号
         ppid = atoi(token);
-      }else{
+      }
+      else
+      {
         fclose(fp);
         perror("File reading error \n");
         exit(1);
       }
       fclose(fp);
-      PROC * temp = (PROC *)malloc(sizeof(PROC));
+      PROC *temp = (PROC *)malloc(sizeof(PROC));
       temp->pid = pid;
       temp->ppid = ppid;
       truncate_right_bracket(comm);
-      strncpy(temp->comm , comm + 1 , 64);
-      printf("pid = %d , name = %s ,ppid = %d\n" , temp->pid ,temp->comm , temp->ppid );
+      strncpy(temp->comm, comm + 1, 64);
+      printf("pid = %d , name = %s ,ppid = %d\n", temp->pid, temp->comm, temp->ppid);
       free(temp);
     }
     dr = readdir(proc);
@@ -158,22 +158,89 @@ int is_digit(char c)
 }
 
 // 用于删除一个字符串中的)
-void truncate_right_bracket(char *s){
-  int i = 0 ;
-  while(s[i] != '\0' && s[i] != ')'){
+void truncate_right_bracket(char *s)
+{
+  int i = 0;
+  while (s[i] != '\0' && s[i] != ')')
+  {
     i++;
   }
   s[i] = '\0';
 }
 
 // 寻找一个进程号为pid的proc
-PROC * find_proc(pid_t pid){
+PROC *find_proc(pid_t pid)
+{
   PROC *temp = list;
-  while(temp){
-    if(temp->pid == pid){
+  while (temp)
+  {
+    if (temp->pid == pid)
+    {
       return temp;
     }
     temp = temp->next;
   }
   return NULL;
+}
+
+// 创建一个新的proc 添加到list中,然后返回相应的指针
+PROC *new_proc(char *comm, pid_t pid, pid_t ppid)
+{
+  PROC *new = (PROC *)malloc(sizeof(PROC));
+  memset(new, 0, sizeof(PROC));
+  strncpy(new->comm, comm, 64);
+  new->pid = pid;
+  new->ppid = ppid;
+  new->next = list;
+  list = new;
+}
+
+/**
+ * @brief 把子进程加入到父进程的children中
+ *
+ * @param p 父进程
+ * @param c 子进程
+ */
+void add_child(PROC *p, PROC *c)
+{
+  CHILD *new_child = (CHILD *)malloc(sizeof(CHILD));
+  memset(new_child, 0, sizeof(CHILD));
+  new_child->child = c;
+  CHILD *cur = p->children;
+  CHILD *prev = NULL;
+  if (p->children == NULL)
+  {
+    // 当前还没有children
+    p->children = new_child;
+  }
+  else
+  {
+    if (n)
+    {
+      // 需要按照pid从小到大排列
+      while (cur != NULL && cur->child->pid < c->pid)
+      {
+        prev = cur;
+        cur = cur->next;
+      }
+      if(prev == NULL){
+        // 插入到最左端
+        new_child->next = cur;
+        p->children = new_child;
+      }else if(cur == NULL){
+        // 插入到最右端
+        prev->next = new_child;
+      }else{
+        // 插入到prev , cur之间
+        new_child->next = cur;
+        prev->next = new_child;
+      }
+
+    }
+    else
+    {
+      new_child->next = cur;
+      p->children = new_child;
+    }
+  }
 }
