be50e45ebbe2d65b4e34a57369388e71cb7b8f5c
tracer-nju <tracer@nju.edu.cn>
2022-07-09 22:53:17 +0800

    Linux jasonj2021-virtual-machine 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:53:17 up  2:36,  4 users,  load average: 0.07, 0.05, 0.01

--- njuOS/os-workbench-2022/pstree/pstree.c
+++ njuOS/os-workbench-2022/pstree/pstree.c
@@ -100,6 +100,7 @@ int main(int argc, char *argv[])
       PROC * temp = (PROC *)malloc(sizeof(PROC));
       temp->pid = pid;
       temp->ppid = ppid;
+      truncate(comm);
       strncpy(temp->comm , comm + 1 , 64);
       printf("pid = %d , name = %s ,ppid = %d\n" , temp->pid ,temp->comm , temp->ppid );
       free(temp);
@@ -139,3 +140,11 @@ int is_digit(char c)
   return 0;
 }
 
+// 用于删除一个字符串中的)
+void truncate(char *s){
+  int i = 0 ;
+  while(s[i] != '\0' && s[i] != ')'){
+    i++;
+  }
+  s[i] = '\0';
+}
