f25ebecf36030dec47fcf2b104a30895e21d17b2
tracer-nju <tracer@nju.edu.cn>
2022-07-10 20:48:13 +0800

    Linux jasonj2021-virtual-machine 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:48:13 up  1:22,  3 users,  load average: 0.03, 0.03, 0.00

--- njuOS/os-workbench-2022/pstree/pstree.c
+++ njuOS/os-workbench-2022/pstree/pstree.c
@@ -294,7 +294,7 @@ void modify_proc(PROC *proc, char *comm, pid_t pid, pid_t ppid)
 char *print_tree()
 {
   int indent = 0;
-  volatile char *ans = NULL;
+  char *ans = NULL;
   print_tree_helper(init, indent, ans);
   return ans;
 }
