f1a685b97b67233f1a13ff95f004bb8738d33271
tracer-nju <tracer@nju.edu.cn>
2022-07-14 16:54:32 +0800

    Linux jasonj2021-virtual-machine 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:32 up  3:13,  2 users,  load average: 0.12, 0.04, 0.01

--- njuOS/os-workbench-2022/libco/co.c
+++ njuOS/os-workbench-2022/libco/co.c
@@ -72,17 +72,17 @@ void co_yield ()
       // 如果新创建还没有运行的，需要进行堆栈的切换
       next_co_ptr->status = CO_RUNNING;
 
-      asm volatile(
-#if __x86_64__
-          "movq %%rdi, 0(%0); movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
-          :
-          : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 16), "d"((uintptr_t)next_co_ptr->func), "a"(next_co_ptr->arg)
-#else
-          "movl %%edi, 4(%0); movl %0, %%esp; movl %2, 0(%0); jmp *%1"
-          :
-          : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 8), "d"((uintptr_t)next_co_ptr->func, "a"(next_co_ptr->arg))
-#endif
-      );
+//       asm volatile(
+// #if __x86_64__
+//           "movq %%rdi, 0(%0); movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+//           :
+//           : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 16), "d"((uintptr_t)next_co_ptr->func), "a"(next_co_ptr->arg)
+// #else
+//           "movl %%edi, 4(%0); movl %0, %%esp; movl %2, 0(%0); jmp *%1"
+//           :
+//           : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 8), "d"((uintptr_t)next_co_ptr->func, "a"(next_co_ptr->arg))
+// #endif
+//       );
       //       asm volatile(
       // #if __x86_64__
       //           "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
