39be0b159672cbeb85a68832fd5ad7e9afcf87d7
tracer-nju <tracer@nju.edu.cn>
2022-07-14 14:28:40 +0800

    Linux jasonj2021-virtual-machine 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:28:40 up 47 min,  1 user,  load average: 0.02, 0.05, 0.00

--- njuOS/os-workbench-2022/libco/co.c
+++ njuOS/os-workbench-2022/libco/co.c
@@ -1,17 +1,151 @@
 #include "co.h"
 #include <stdlib.h>
 #include <setjmp.h>
+#include <stdint.h>
+#include <string.h>
 
+#define STACK_SIZE 8192
+#define CO_SIZE 128
 
-struct co {
+void co_init() __attribute__((constructor));
+
+enum co_status
+{
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
 };
 
-struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+struct co
+{
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+
+  enum co_status status; // 协程的状态
+  // struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf context;           // 寄存器现场 (setjmp.h)
+  int loc;  // 位于数组的位置  
+  uint8_t stack[STACK_SIZE]; // 协程的堆栈
+};
+
+int cur_co = 0;
+struct co *co_list[CO_SIZE];
+
+struct co *co_start(const char *name, void (*func)(void *), void *arg)
+{
+  struct co *new_co = (struct co *)malloc(sizeof(struct co));
+  new_co->name = name;
+  new_co->func = func;
+  new_co->arg = arg;
+  new_co->status = CO_NEW;
+  int free_spot = find_free_spot();
+  co_list[free_spot] = new_co;
+  new_co->loc = free_spot;
+  return new_co;
 }
 
-void co_wait(struct co *co) {
+void co_wait(struct co *co)
+{
+  while(co->status != CO_DEAD){
+    co_yield();
+  }
+
+  free(co);
+  co_list[co->loc] = NULL;
+  
 }
 
-void co_yield() {
+void co_yield ()
+{
+  int val = setjmp(current->context);
+  if (val == 0)
+  {
+    // 保存完寄存器现场之后，我们需要选择下一个协程进行切换
+    cur_co = find_next_co();
+    struct co *next_co_ptr = co_list[cur_co];
+    if (next_co_ptr->status == CO_NEW)
+    {
+      // 如果新创建还没有运行的，需要进行堆栈的切换
+      asm volatile(
+#if __x86_64__
+          "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
+          :
+          : "b"((uintptr_t)(next_co_ptr->stack + STACK_SIZE)), "d"(next_co_ptr->func), "a"(next_co_ptr->arg)
+          : "memory"
+#else
+          "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
+          :
+          : "b"((uintptr_t)(next_co_ptr->stack + STACK_SIZE - 8)), "d"(next_co_ptr->func), "a"(next_co_ptr->arg)
+          : "memory"
+#endif
+      );
+      // 此时函数已经运行完毕
+      next_co_ptr->status = CO_DEAD;
+      co_yield();
+    }
+    else
+    {
+      // 如果已经运行了，直接进行longjmp
+      longjmp( next_co_ptr->context, 8);
+    }
+  }
+  else
+  {
+    return;
+  }
+}
+
+void co_init()
+{
+  // 初始化main 的协程
+  for (int i = 0; i < CO_SIZE; i++)
+  {
+    co_list[i] = NULL;
+  }
+  struct co *main_co = (struct co *)malloc(sizeof(struct co));
+  main_co->name = "main_co";
+  main_co->func = NULL;
+  main_co->arg = NULL;
+  main_co->status = CO_RUNNING;
+  main_co->loc = 0;
+  // 把main协程添加到协程list中
+  co_list[cur_co] = main_co;
+}
+
+// 在co_list中寻找一个空的插槽
+int find_free_spot()
+{
+  for (int i = 0; i < CO_SIZE; i++)
+  {
+    if (co_list[i] == NULL)
+    {
+      return i;
+    }
+  }
+  // 根据实验手册不可能出现这种情况
+  return -1;
+}
+
+// 在co_list中寻找下一个运行的协程
+int find_next_co()
+{
+  for (int i = cur_co + 1; i < CO_SIZE; i++)
+  {
+    if (co_list[i] != NULL && co_list[i]->status != CO_DEAD)
+    {
+      return i;
+    }
+  }
+  for (int i = 0; i < cur_co; i++)
+  {
+    if (co_list[i] != NULL && co_list[i]->status != CO_DEAD)
+    {
+      return i;
+    }
+  }
+  // 一般不会出现这种情况
+  return -1;
 }
