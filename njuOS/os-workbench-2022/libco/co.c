d6ec897fec9e0e68efccc04712f9d38a1294d2f9
tracer-nju <tracer@nju.edu.cn>
2022-07-14 16:55:48 +0800

    Linux jasonj2021-virtual-machine 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:55:48 up  3:15,  2 users,  load average: 0.09, 0.06, 0.01

--- njuOS/os-workbench-2022/libco/co.c
+++ njuOS/os-workbench-2022/libco/co.c
@@ -81,7 +81,7 @@ void co_yield ()
 #else
           "movl %%edi, 4(%0); movl %0, %%esp; movl %2, 0(%0); jmp *%1"
           :
-          : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 8), "d"(next_co_ptr->func, "a"(next_co_ptr->arg))
+          : "b"((uintptr_t)next_co_ptr->stack + STACK_SIZE - 8), "d"(next_co_ptr->func), "a"(next_co_ptr->arg))
           :"memory"
 #endif
       );
